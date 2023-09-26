100798e253f2fd4c620d5d84c6fdfa392d2b0c17
VICTOR MERSEGUEL <merseguel@gmail.com>
2023-09-26 11:31:16 +0100

    creats `background animation` and starts style

---
+++ js/main.js
@@ -0,0 +1,24 @@
+import { sketch } from "./sketch.js";
+
+const title = document.querySelector(".title-mask");
+const mask = document.querySelector(".mask");
+
+// functions
+
+const animate = () => {
+  sketch();
+  requestAnimationFrame(animate);
+};
+
+animate();
+
+// Mask start
+setTimeout(() => {
+  title.style.opacity = "0";
+  setTimeout(() => {
+    mask.style.opacity = "0";
+  }, 1000);
+}, 1500);
+setTimeout(() => {
+  mask.style.display = "none";
+}, 3500);
---
+++ js/sketch.js
@@ -0,0 +1,109 @@
+import { Agent, backColor, color } from "./Agents.js";
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+let { width, height } = canvasSketch();
+let agents = [];
+
+window.addEventListener("resize", sketch, false);
+
+const randomRange = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+
+function mapRange(value, inMin, inMax, outMin, outMax) {
+  return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+function canvasSketch() {
+  canvas.height = window.innerHeight;
+  window.innerWidth > 1000
+    ? (canvas.width = 1000)
+    : (canvas.width = window.innerWidth);
+
+  let width = canvas.width;
+  let height = canvas.height;
+
+  //   sketch(width, height);
+  return { width, height };
+}
+
+const createAgents = () => {
+  const numberOfAgents = (width * 2) / 10;
+  for (let i = 0; i < numberOfAgents; i++) {
+    const x = randomRange(0, width);
+    const y = randomRange(0, height);
+
+    agents.push(new Agent(x, y));
+  }
+};
+
+createAgents();
+
+function sketch() {
+  // clear canvas with full white rectangle
+  context.save();
+  context.fillStyle = backColor;
+
+  context.beginPath();
+  context.rect(0, 0, width, height);
+  context.fill();
+
+  context.restore();
+
+  // resize validation
+  if (window.innerWidth != width || window.innerHeight != height) {
+    if (window.innerWidth > 1000 && width === 1000) {
+    } else if (window.innerWidth > 1000 && width !== 1000) {
+      width = 1000;
+      height = window.innerHeight;
+      agents = [];
+      canvasSketch();
+      createAgents();
+    } else if (window.innerWidth <= 1000) {
+      width = window.innerWidth;
+      height = window.innerHeight;
+      agents = [];
+      canvasSketch();
+      createAgents();
+    }
+  }
+
+  for (let i = 0; i < agents.length; i++) {
+    const agent = agents[i];
+    for (let j = i + 1; j < agents.length; j++) {
+      const other = agents[j];
+      const distBetween = 200;
+
+      const dist = agent.pos.getDistance(other.pos);
+
+      if (dist > distBetween) continue;
+
+      context.save();
+
+      context.lineWidth = mapRange(dist, 0, distBetween, 0.7, 0);
+      context.beginPath();
+      context.moveTo(agent.pos.x, agent.pos.y);
+      context.lineTo(other.pos.x, other.pos.y);
+      context.strokeStyle = color;
+      context.stroke();
+
+      context.restore();
+    }
+  }
+
+  // animation
+  agents.forEach((agent) => {
+    agent.update();
+    if (agent.pos.x <= 0 || agent.pos.x >= width) {
+      agent.vel.x *= -1;
+    }
+    if (agent.pos.y <= 0 || agent.pos.y >= height) {
+      agent.vel.y *= -1;
+    }
+    agent.draw();
+  });
+}
+
+export { sketch, randomRange, context };
