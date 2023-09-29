54645822d4172746b405f534268f1b847167dcc0
VICTOR MERSEGUEL <merseguel@gmail.com>
2023-09-29 17:28:34 +0100

    add files

--- js/events.js
+++ js/events.js
@@ -1,6 +1,8 @@
+// INSTANCES --------------------------------------------
+
 // nav buttons
-const buttons = document.querySelectorAll("nav li a");
-const dots = document.querySelectorAll("nav li span");
+const buttons = document.querySelectorAll("nav li a ");
+const dots = document.querySelectorAll(".dot");
 
 // home
 const saibaMaisBtn = document.querySelector(".saiba-mais");
@@ -10,6 +12,12 @@ const lightBtn = document.querySelector("#light");
 const lightBtnSpan = document.querySelector("#light span");
 const darkBtn = document.querySelector("#dark");
 const darkBtnSpan = document.querySelector("#dark span");
+
+// language buttons
+const ptBtn = document.querySelector("#pt");
+const ptBtnSpan = document.querySelector("#pt span");
+const enBtn = document.querySelector("#en");
+const enBtnSpan = document.querySelector("#en span");
 
 // sections
 const sections = document.querySelectorAll(".section");
@@ -22,51 +30,20 @@ const video = document.querySelector(".project-more-content video");
 
 const projects = document.querySelectorAll(".project");
 
+// VARIABLES --------------------------------------------
 let theme = true;
+let language = "-pt";
+let current = "";
 
+// prettier-ignore
 const states = [
-  {
-    name: "home",
-    status: true,
-  },
-  {
-    name: "projects",
-    status: false,
-  },
-  {
-    name: "info",
-    status: false,
-  },
-  {
-    name: "contact",
-    status: false,
-  },
+  { name: "home", status: true },
+  { name: "projects", status: false },
+  { name: "info", status: false },
+  { name: "contact", status: false },
 ];
 
-const handleNav = (e) => {
-  const id = e.target.id;
-
-  states.forEach((state, ind) => {
-    state.name + "-btn" === id
-      ? (sections[ind].classList.remove("hide"),
-        buttons.forEach((button, i) => {
-          button.id === id
-            ? (button.classList.add("hide"), dots[i].classList.remove("hide"))
-            : (button.classList.remove("hide"), dots[i].classList.add("hide"));
-        }))
-      : sections[ind].classList.add("hide");
-  });
-};
-
-const handleTheme = (e) => {
-  e.target.id === "light"
-    ? ((theme = true),
-      (lightBtnSpan.innerText = "◼"),
-      (darkBtnSpan.innerText = "◻"))
-    : ((theme = false),
-      (lightBtnSpan.innerText = "◻"),
-      (darkBtnSpan.innerText = "◼"));
-};
+// NAVIGATION -------------------------------------------
 
 export const navigate = () => {
   buttons.forEach((button, i) => {
@@ -80,15 +57,40 @@ export const navigate = () => {
   lightBtn.addEventListener("click", (e) => handleTheme(e));
   darkBtn.addEventListener("click", (e) => handleTheme(e));
 
+  ptBtn.addEventListener("click", (e) => handleLanguage(e));
+  enBtn.addEventListener("click", (e) => handleLanguage(e));
+
   projects.forEach((project) => {
     project.addEventListener("click", (e) => {
-      console.log(e);
       projectMoreContainer.classList.remove("hide");
       video.play();
     });
   });
 };
 
+const handleNav = (e) => {
+  const id = e.target.id;
+
+  states.forEach((state, ind) => {
+    state.name + "-btn" + language === id
+      ? (sections[ind].classList.remove("hide"),
+        buttons.forEach((button, i) => {
+          button.id === id
+            ? (button.classList.add("hide"),
+              button.classList.add("current"),
+              dots.forEach((dot) => {
+                dot.classList.add("hide");
+              }),
+              dots[parseInt(i / 2)].classList.remove("hide"),
+              dots[parseInt(i / 2)].classList.remove("current"))
+            : (button.classList.remove("hide"),
+              button.classList.remove("current"));
+        }))
+      : sections[ind].classList.add("hide");
+  });
+};
+
+// PROJET WINDOW -----------------------------------------
 export const closeProjetMore = () => {
   projectMoreContent.addEventListener("click", (e) => {
     e.stopPropagation();
@@ -105,4 +107,66 @@ export const closeProjetMore = () => {
     video.pause();
     video.currentTime = 0;
   });
+};
+
+// LANGUAGE ----------------------------------------------
+const languageSelection = () => {
+  language === "-pt"
+    ? (document.querySelectorAll(".pt").forEach((item) => {
+        item.classList.remove("hideLanguage");
+      }),
+      document.querySelectorAll(".en").forEach((item) => {
+        item.classList.add("hideLanguage");
+      }))
+    : (document.querySelectorAll(".pt").forEach((item) => {
+        item.classList.add("hideLanguage");
+      }),
+      document.querySelectorAll(".en").forEach((item) => {
+        item.classList.remove("hideLanguage");
+      }));
+};
+
+languageSelection();
+const handleLanguage = (e) => {
+  let currentButton = document.querySelector(".current");
+  let currentSliced;
+
+  currentButton
+    ? (currentSliced = currentButton.id.slice(0, currentButton.id.length - 3))
+    : (currentSliced = "home-btn");
+
+  e.target.id === "pt"
+    ? ((language = "-pt"),
+      (ptBtnSpan.innerText = "◼"),
+      (enBtnSpan.innerText = "◻"),
+      (current = currentSliced + language))
+    : ((language = "-en"),
+      (ptBtnSpan.innerText = "◻"),
+      (enBtnSpan.innerText = "◼"),
+      (current = currentSliced + language));
+
+  buttons.forEach((button) => {
+    button.id === current ? button.classList.add("hide") : null;
+  });
+
+  languageSelection();
+};
+
+// THEME --------------------------------------------------
+const handleTheme = (e) => {
+  e.target.id === "light"
+    ? ((theme = true),
+      document.querySelectorAll(".dark").forEach((item) => {
+        item.classList.add("light");
+        item.classList.remove("dark");
+      }),
+      (lightBtnSpan.innerText = "◼"),
+      (darkBtnSpan.innerText = "◻"))
+    : ((theme = false),
+      document.querySelectorAll(".light").forEach((item) => {
+        item.classList.add("dark");
+        item.classList.remove("light");
+      }),
+      (lightBtnSpan.innerText = "◻"),
+      (darkBtnSpan.innerText = "◼"));
 };
